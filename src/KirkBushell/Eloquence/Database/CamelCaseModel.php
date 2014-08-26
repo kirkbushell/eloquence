3eaab5adb2faa7cb1147d2c7c2764de7f597244d
kirkbushell <torm3nt@gmail.com>
2014-08-26 11:26:15 +0100

    Eloquence now supports camelcasing via a trait.

---
+++ src/KirkBushell/Eloquence/Database/CamelCaseModel.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace KirkBushell\Eloquence\Database;
+
+trait CamelCaseModel
+{
+	/**
+	 * Alter eloquent model behaviour so that model attributes can be accessed via camelCase, but more importantly,
+	 * attributes also get returned as camelCase fields.
+	 *
+	 * @var bool
+	 */
+	public $enforceCamelCase = true;
+
+	/**
+	 * Overloads the eloquent setAttribute method to ensure that fields accessed
+	 * in any case are converted to snake_case, which is the defacto standard
+	 * for field names in databases.
+	 *
+	 * @param string $key
+	 * @param mixed $value
+	 */
+	public function setAttribute($key, $value)
+	{
+		parent::setAttribute($this->getTrueKey($key), $value);
+	}
+
+	/**
+	 * Retrieve a given attribute but allow it to be accessed via alternative case methods (such as camelCase).
+	 *
+	 * @param string $key
+	 * @return mixed
+	 */
+	public function getAttribute($key)
+	{
+		return parent::getAttribute($this->getTrueKey($key));
+	}
+
+	/**
+	 * Return the attributes for the model, converting field casing if necessary.
+	 *
+	 * @return array
+	 */
+	public function attributesToArray()
+	{
+		$attributes = parent::attributesToArray();
+		$convertedAttributes = [];
+
+		foreach ($attributes as $key => $value) {
+			if ($this->enforceCamelCase) {
+				$key = camel_case($key);
+			}
+
+			$convertedAttributes[$key] = $value;
+		}
+
+		return $convertedAttributes;
+	}
+
+	/**
+	 * If the field names need to be converted so that they can be accessed by camelCase, then we can do that here.
+	 *
+	 * @param $key
+	 * @return string
+	 */
+	protected function getTrueKey($key)
+	{
+		return snake_case($key);
+	}
+}
