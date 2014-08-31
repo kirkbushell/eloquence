94dd626080914558fe04bed2706720395b16a2c9
kirkbushell <torm3nt@gmail.com>
2014-08-31 23:23:01 +0200

    Initial tests now working.

--- Tests/Database/Traits/CamelCaseModelTest.php
+++ Tests/Database/Traits/CamelCaseModelTest.php
@@ -6,14 +6,27 @@ use Tests\Stubs\ModelStub;
 
 class CamelCaseModelTest extends \PHPUnit_Framework_TestCase
 {
+    private $model;
+
+    public function setUp()
+    {
+        $this->model = new ModelStub;
+    }
+
     public function testAttributesRetrieval()
     {
-        $model = new ModelStub;
-        $attributes = $model->attributesToArray();
+        $attributes = $this->model->attributesToArray();
 
         $this->assertArrayHasKey('firstName', $attributes);
         $this->assertArrayHasKey('lastName', $attributes);
         $this->assertArrayHasKey('address', $attributes);
         $this->assertArrayHasKey('firstName', $attributes);
+    }
+
+    public function testAttributeDeclaration()
+    {
+        $this->model->setAttribute('firstName', 'Andrew');
+
+        $this->assertEquals('Andrew', $this->model->getAttribute('firstName'));
     }
 }
